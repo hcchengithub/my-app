784a3fc3812e3f7360fc222558f461d410ce3d3a
H.C. Chen <hcchen5600@gmail.com>
2025-12-26 02:22:57 +0800

    refactor: Switch to "None" template with Express server
    This commit refactors the application to remove the Webpack template and its associated dependencies. The goal is to
    simplify the architecture into a lightweight Electron background process that serves the front-end via a self-contained
    Express web server.
    
    - Removed all Webpack-related configurations and loader packages.
    - Reconfigured forge.config.js to remove the webpack plugin.
    - Re-implemented the main process (main.js) to launch an Express server that hosts static front-end files.
    - Updated package.json to reflect the new, simpler dependency tree and bumped the version to 1.1.0.

--- src/main.js
+++ src/main.js
@@ -1,39 +1,62 @@
-const { app, BrowserWindow } = require('electron');
+const { app } = require('electron');
 const path = require('node:path');
+const express = require('express');
+const open = require('open');
+const portfinder = require('portfinder');
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
   app.quit();
 }
 
-const createWindow = () => {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY,
-    },
+let server; // To hold the server instance
+
+const startWebServer = async () => {
+  const expressApp = express();
+
+  // Serve static files from the 'src' directory (where index.html, renderer.js, index.css are)
+  // __dirname in main.js is C:\Users\8304018\Documents\GitHub\Electron\my-app\src
+  expressApp.use(express.static(path.join(__dirname)));
+
+  // Find an available port, starting from 3000
+  const port = await portfinder.getPortPromise({
+    port: 3000,
+    stopPort: 4000
   });
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
+  const url = `http://localhost:${port}`;
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  server = expressApp.listen(port, () => {
+    console.log(`Web server running at ${url}`);
+    // Handle CommonJS/ESM module interoperability for 'open' package
+    if (typeof open === 'function') {
+      open(url);
+    } else {
+      open.default(url);
+    }
+  });
+};
+
+const initializeApp = () => {
+  startWebServer();
 };
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  createWindow();
+  initializeApp();
 
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
+    // On macOS, if the app is activated (e.g., dock icon clicked)
+    // and the server is running, re-open the browser to the URL.
+    if (server) {
+      const url = `http://localhost:${server.address().port}`;
+      if (typeof open === 'function') {
+        open(url);
+      } else {
+        open.default(url);
+      }
     }
   });
 });
@@ -42,8 +65,20 @@ app.whenReady().then(() => {
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
+  // In our web server model, the app runs in the background.
+  // It shouldn't quit just because there are no "windows".
+  // On macOS, it stays active by default. On other platforms, we prevent quitting.
   if (process.platform !== 'darwin') {
-    app.quit();
+    // If you want the app to genuinely quit when not in use, you'd implement a tray icon
+    // or other UI to allow the user to explicitly quit.
+    // For now, we'll keep it running in background.
+  }
+});
+
+app.on('before-quit', () => {
+  if (server) {
+    server.close();
+    console.log('Web server is shutting down.');
   }
 });
 
